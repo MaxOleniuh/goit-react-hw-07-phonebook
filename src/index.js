561ac4e3d35a335321fd8863d81d3420ffce8478
oleniuh <maxim.oleniukh@gmail.com>
2023-05-19 14:03:06 +0100

    persistor removed

--- src/index.js
+++ src/index.js
@@ -2,24 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'App/App';
 import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store';
+import { store } from './redux/store';
 import './index.css';
-import { PersistGate } from 'redux-persist/integration/react';
 import { MantineProvider } from '@mantine/core';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <MantineProvider
-          withNormalizeCSS
-          withGlobalStyles
-          theme={{ colorScheme: 'dark' }}
-        >
-          {' '}
-          <App />
-        </MantineProvider>
-      </PersistGate>
+      <MantineProvider
+        withNormalizeCSS
+        withGlobalStyles
+        theme={{ colorScheme: 'dark' }}
+      >
+        {' '}
+        <App />
+      </MantineProvider>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,32 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import slice from './slice';
 import filterSlice from './filterSlice';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-const persistConfig = {
-  key: 'toolkit',
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, slice);
-
+import slice from './slice';
 export const store = configureStore({
-  reducer: { toolkit: persistedReducer, filter: filterSlice },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: {
+    toolkit: slice,
+    filter: filterSlice,
+  },
 });
-export const persistor = persistStore(store);
